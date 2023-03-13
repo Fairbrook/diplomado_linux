c36ba3558e690e74928bd2659eddefa10f7c385f
Kevin Martinez <kevinvr@hotmail.es>
2023-03-13 13:55:07 -0600

    psuedo shell

--- pseudo_shell_Kevin_Martinez.c
+++ pseudo_shell_Kevin_Martinez.c
@@ -1,46 +1,54 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <sys/types.h>
+#include <unistd.h>
+char** psuedo_to_real(int, char *[]);
 
 int main(int argc, char *argv[]){
     if(argc<2){
         printf("No input command detected\n");
         return EXIT_FAILURE;
     }
-     
+    char **command = psuedo_to_real(argc, argv);
+    if(command[0]==NULL){
+        printf("Invalid command '%s'\n", argv[1]);
+        printf("Try fecha, quiensoy, tiempo or personal\n");
+        return EXIT_FAILURE;
+    }
+    int exec_return = execvp(command[0], command);
+    if(exec_return == -1){
+        perror("Error al ejecutar el comando");
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
 
-char **get_extra_args(int argc, char *argv[]){
-    int extra_len = argc-2+1;
-    char **args = (char**) malloc(extra_len * sizeof(char*));
-    int argi = 0;
-    for (int i=2;i<argc;i++, argi++){
-        args[argi] = argv[i];
+char **allocate_array(unsigned int len){
+    char **array = (char**)malloc(len);
+    for(int i=0;i<len;i++){
+        array[i] = NULL;
     }
-    args[argi] = NULL;
-    return args;
-}
-
-char **append(char *append,int len, char *vector[]){
-    char *item = vector[0];
-    char **result_vector = (char**) malloc(len+1 *sizeof(char*));
-    result_vector[0] = append;
-    for (int i=0;i<len;i++){
-        result_vector[i+1] = vector[i];
-    }
-    return result_vector;
+    return array;
 }
 
 char** psuedo_to_real(int argc, char *argv[]){
     char *psuedo = argv[1];
     if(strcmp(psuedo, "fecha")==0){
-        return append("date", argc-1, get_extra_args(argc,argv));
+        char **date = allocate_array(2);
+        date[0] = "date";
+        return date;
     }
     if(strcmp(psuedo, "quiensoy")==0){
-        return append("whoiam", argc-1, get_extra_args(argc,argv));
+        char **whoami = allocate_array(2);
+        whoami[0] = "whoami";
+        return whoami;
     }
     if(strcmp(psuedo, "tiempo")==0){
-        return append("uptime", argc-1, get_extra_args(argc,argv));
+        char **uptime = allocate_array(3);
+        uptime[0] = "uptime";
+        uptime[1] = "-p";
+        return uptime;
     }
+    return allocate_array(1);
 }
