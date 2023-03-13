35094d5ecdcd6035fb02e3ec3cc06c7c73907207
Fairbrook <kevinvr@hotmail.es>
2023-03-13 08:43:33 -0600

    forkexec

---
+++ forkexec.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int spawn(char *program, char **arg_list) {
+  pid_t child_pid;
+
+  child_pid = fork();
+  if (child_pid != 0)
+
+    return child_pid;
+  else {
+    execvp(program, arg_list);
+    printf("returning from execvp");
+    abort();
+  }
+}
+int main() {
+  char *arg_list[] = {"ls\0", "-l\0", "/\0", NULL};
+  spawn("ls", arg_list);
+  printf("done with main program\n");
+  return 0;
+}
