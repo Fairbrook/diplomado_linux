1aea1e0aea5f694403bbc274ba740fa597a1109f
Fairbrook <kevinvr@hotmail.es>
2023-03-13 15:32:27 -0600

    race condition

--- threads_race_condition_fixed_Kevin_Martinez.c
+++ threads_race_condition_fixed_Kevin_Martinez.c
@@ -2,15 +2,18 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int x = 0;
+int counter = 0;
+pthread_mutex_t counter_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void* thread_func(void* args){
 
     int i;
+    pthread_mutex_lock(&counter_mutex);
     for ( i = 0; i < 10000000; i++ )
     {
-        x++; // errata from the processes class: this is not atomic
+        counter++;
     }
+    pthread_mutex_unlock(&counter_mutex);
     return NULL;
 }
 
@@ -18,14 +21,14 @@ int main () {
     pthread_t thread_id[2];
 
     printf("I will create two threads\n");
-    printf("Point 1 >> X is: %d\n", x);
+    printf("Point 1 >> X is: %d\n", counter);
     pthread_create(&thread_id[0],NULL,&thread_func,NULL);
     pthread_create(&thread_id[1],NULL,&thread_func,NULL);
     printf("The threads has been created\n");
     pthread_join(thread_id[0],NULL);
     pthread_join(thread_id[1],NULL);
     printf("The threads have exited\n");
-    printf("Point 2 >> X is: %d\n", x);
-
+    printf("Point 2 >> X is: %d\n", counter);
+    pthread_exit(NULL);
     return 0;
 }
